c979901b3107ab7b6ae6fd315a4e0b8ca6c24046
Mihail Chifligarov <mihail9512@yahoo.co.uk>
2024-01-09 11:49:24 +0100

    Week 10 init

---
+++ Week10/5_ueb_cosine_similarity.py
@@ -0,0 +1,42 @@
+"""
+Aufgabenbeschreibung: 
+Implementieren Sie das Beispiel von S.113 aus Jurafsky & Martin. Dabei soll die Kosinus-Ähnlichkeit zwischen
+zwei Wörtern berechnet werden. Nutzen Sie die angegebene Term-Term-Matrix zur Erstellung der Vektoren und berechnen
+Sie als Test die Kosinus-Ähnlichkeit zwischen "cherry" und "information" einerseits und
+"digital" und "information" andererseits. Achtung: Der Code soll aber so allgemein geschrieben sein, dass auch andere
+Wortpaare miteinander verglichen werden können und dass auch eine andere Term-Term-Matrix eingesetzt werden könnte.
+Sie dürfen keine externen Libraries außer math benutzen.
+"""
+
+import math
+
+################################
+# Funktionen
+################################
+
+
+
+
+# Funktion, die alle weiteren Funktionen aufruft
+def run_script():
+    """Funktion, die alle weiteren Funktionen aufruft"""
+    
+    pass
+
+
+################################
+# Hauptprogramm
+################################
+
+if __name__ == "__main__":
+
+    
+    #### Inputdaten = Term-Term-Matrix auf Seite 113
+    
+    ## Todo: Geeignetes Format finden
+    
+    
+    run_script()
+
+
+
